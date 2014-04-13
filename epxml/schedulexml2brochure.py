a25e34a9fe4fac0e4713023a5bac476ca5f45899
Andreas Jung <yet@gmx.de>
2014-04-13 11:25:07 -0400

    PDF generation integrated

--- epxml/schedulexml2brochure.py
+++ epxml/schedulexml2brochure.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 import plac
+import subprocess
+import loremipsum
 from jinja2 import Environment, PackageLoader
 import util
-import loremipsum
-
 
 
 env = Environment(loader=PackageLoader('epxml', 'templates'))
@@ -19,8 +20,9 @@ class View(object):
 @plac.annotations(
     xml_in=('Schedule XML file', 'option', 'i'),
     html_out=('Output HTML file', 'option', 'o'),
+    pdf_converter=('Generate PDF output using prince or pdfreactor', 'option', 'p')
     )
-def conv(xml_in, html_out='brochure.html'):
+def conv(xml_in, html_out='brochure.html', pdf_converter=None):
     entries_d1 = util.get_entries(xml_in, '//day[@date="2014-07-22"]/entry')
     entries_d2 = util.get_entries(xml_in, '//day[@date="2014-07-23"]/entry')
     entries_d3 = util.get_entries(xml_in, '//day[@date="2014-07-24"]/entry')
@@ -31,8 +33,20 @@ def conv(xml_in, html_out='brochure.html'):
             d3=dict(entries=entries_d3, date_str='24/07/2014'),
             view=View())
     with open(html_out, 'wb') as fp:
+        print 'HTML output written to "{}"'.format(html_out)
         fp.write(html.encode('utf8'))
 
+    if pdf_converter in ('prince', 'pdfreactor'):
+        out_pdf = '{}.pdf'.format(os.path.splitext(html_out)[0])
+        if pdf_converter == 'prince':
+            cmd = 'prince9 "{}" -o "{}"'.format(html_out, out_pdf)
+        elif pdf_converter == 'pdfreactor':
+            cmd = 'pdfreactor "{}" "{}"'.format(html_out, out_pdf)
+        print 'Running: {}'.format(cmd)
+        proc = subprocess.Popen(cmd, shell=True)
+        status = proc.wait()
+        print 'Exit code: {}'.format(status)
+        print 'PDF written to "{}"'.format(out_pdf)
 
 def main():
     plac.call(conv)
